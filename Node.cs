1d2cb64fefa430ddb76b75994bdaa04da51890ac
Batuhan AVLAYAN <b.avlayan@gmail.com>
2020-10-20 20:55:22 +0300

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTree
+{
+    public class Node
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Node> Childs { get; set; }
+
+        public Node(string _name, int _id)
+        {
+            this.Name = _name;
+            this.Id = _id;
+
+            Childs = new List<Node>();            
+        }
+        
+    }
+}
